1dc2f2afd8e79e73066632f1bc8c6f9667cb59db
Valentin Robert <valentin.robert.42@gmail.com>
2020-05-05 14:12:49 +0200

    remove obsolete overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,5 @@
 {
   advent-of-code-2019     = import ./advent-of-code-2019;
-  case                    = import ./case;
   haskell-dev             = import ./haskell-dev; # NOTE: collection of overlays keyed by the ghcVersion
   haskell-language-server = import ./haskell-language-server;
   PUMPKIN-PATCH           = import ./PUMPKIN-PATCH;

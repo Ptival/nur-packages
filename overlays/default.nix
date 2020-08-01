8b0ba8ca4761319df1f90ebf49ea02867def6e2a
Valentin Robert <valentin.robert.42@gmail.com>
2020-08-01 18:41:08 +0200

    remove overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,4 @@
 {
-  advent-of-code-2019 = import ./advent-of-code-2019;
   haskell-dev         = import ./haskell-dev; # NOTE: collection of overlays keyed by the ghcVersion
   PUMPKIN-PATCH       = import ./PUMPKIN-PATCH;
 }
